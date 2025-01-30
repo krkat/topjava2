3ef8d596abf16ed609c5b72a7ea6dffa830c0063
krkat <98359411+EmmaFranBengtsbo@users.noreply.github.com>
2025-01-30 15:12:00 +0300

    HW03 Add meals table initialization and population scripts

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_role;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -10,3 +11,10 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, datetime, description, calories)
+VALUES (100000, '2025-01-18 07:00:00', 'Завтрак', 500),
+       (100000, '2025-01-18 10:00:00', 'Обед', 1000),
+       (100000, '2025-01-18 17:00:00', 'Ужин', 501),
+       (100001, '2025-01-18 07:00:00', 'Завтрак', 600),
+       (100001, '2025-01-18 12:00:00', 'Перекус', 100);
