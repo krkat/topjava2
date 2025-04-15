49e648b33f2e267cb3b6441ab0393e5bc4324c63
krkat <98359411+EmmaFranBengtsbo@users.noreply.github.com>
2025-04-15 17:03:22 +0300

    Create JspMealController

---
+++ src/main/java/ru/javawebinar/topjava/web/JspMealController.java
@@ -0,0 +1,85 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.web.meal.AbstractMealController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
+
+@Controller
+@RequestMapping("/meals")
+public class JspMealController extends AbstractMealController {
+
+    private static final Logger log = LoggerFactory.getLogger(JspMealController.class);
+
+    public JspMealController(MealService service) {
+        super(service);
+    }
+
+    @PostMapping
+    public String setMeal(HttpServletRequest request) {
+        Meal meal = new Meal(
+                LocalDateTime.parse(request.getParameter("dateTime")),
+                request.getParameter("description"),
+                Integer.parseInt(request.getParameter("calories")));
+
+        if (StringUtils.hasLength(request.getParameter("id"))) {
+            update(meal, getId(request));
+        } else {
+            create(meal);
+        }
+        return "redirect:meals";
+    }
+
+    @GetMapping
+    public String getMeals(HttpServletRequest request) {
+        String action = request.getParameter("action");
+
+        switch (action == null ? "all" : action) {
+            case "delete" -> {
+                int id = getId(request);
+                delete(id);
+                return "redirect:meals";
+            }
+            case "create", "update" -> {
+                final Meal meal = "create".equals(action) ?
+                        new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000) :
+                        get(getId(request));
+                request.setAttribute("meal", meal);
+                return "mealForm";
+            }
+            case "filter" -> {
+                LocalDate startDate = parseLocalDate(request.getParameter("startDate"));
+                LocalDate endDate = parseLocalDate(request.getParameter("endDate"));
+                LocalTime startTime = parseLocalTime(request.getParameter("startTime"));
+                LocalTime endTime = parseLocalTime(request.getParameter("endTime"));
+                request.setAttribute("meals", getBetween(startDate, startTime, endDate, endTime));
+                return "meals";
+            }
+            default -> {
+                request.setAttribute("meals", getAll());
+                return "meals";
+            }
+        }
+    }
+
+    private int getId(HttpServletRequest request) {
+        String paramId = Objects.requireNonNull(request.getParameter("id"));
+        return Integer.parseInt(paramId);
+    }
+}
