09fea2d8b845ab67170002d8625b19d2158fa83b
krkat <98359411+EmmaFranBengtsbo@users.noreply.github.com>
2025-04-25 14:33:18 +0300

    Fix MealTo serialization

--- src/main/java/ru/javawebinar/topjava/to/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/to/MealTo.java
@@ -1,22 +1,23 @@
 package ru.javawebinar.topjava.to;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.LocalDateTime;
 
 public class MealTo {
-    private Integer id;
+    private final Integer id;
 
-    private LocalDateTime dateTime;
+    private final LocalDateTime dateTime;
 
-    private String description;
+    private final String description;
 
-    private int calories;
+    private final int calories;
 
-    private boolean excess;
+    private final boolean excess;
 
-    public MealTo() {
-    }
-
-    public MealTo(Integer id, LocalDateTime dateTime, String description, int calories, boolean excess) {
+    @JsonCreator
+    public MealTo(@JsonProperty Integer id, @JsonProperty LocalDateTime dateTime, @JsonProperty String description, @JsonProperty int calories, @JsonProperty boolean excess) {
         this.id = id;
         this.dateTime = dateTime;
         this.description = description;
