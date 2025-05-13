5394b7f0090dfe71c039fd82b2e9a256667bef7f
krkat <98359411+EmmaFranBengtsbo@users.noreply.github.com>
2025-05-13 18:45:02 +0300

    Validation in AdminUIController & MealUIController via ExceptionInfoHandler

--- src/main/java/ru/javawebinar/topjava/util/exception/ErrorType.java
+++ src/main/java/ru/javawebinar/topjava/util/exception/ErrorType.java
@@ -1,8 +1,18 @@
 package ru.javawebinar.topjava.util.exception;
 
 public enum ErrorType {
-    APP_ERROR,
-    DATA_NOT_FOUND,
-    DATA_ERROR,
-    VALIDATION_ERROR
+    APP_ERROR("Ошибка приложения"),
+    DATA_NOT_FOUND("Данные не найдены"),
+    DATA_ERROR("Ошибка данных"),
+    VALIDATION_ERROR("Ошибка проверки данных");
+
+    public final String title;
+
+    ErrorType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
